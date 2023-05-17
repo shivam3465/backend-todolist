006e97983619c1680b17282464017bae0bf732e4
shivam kumar <shivamkr19102000@gmail.com>
2023-05-17 10:56:17 +0530

    added frontedn url

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin:'http://localhost:3000/',   
+    origin:[process.env.frontend_url],   
     credentials: true, 
     methods:['GET', 'POST','PUT','DELETE']
 }))
