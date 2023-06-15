691f2b6f2cba70b59ff25ddfb703d3f1b45dd851
shivam kumar <shivamkr19102000@gmail.com>
2023-06-15 17:19:03 +0530

    fixing bugs in cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin: process.env.frontend_url,       
+    origin: process.env.FRONTEND_URL,       
     methods:['GET', 'POST','PUT','DELETE'],
     credentials: true
 }))
