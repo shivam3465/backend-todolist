fecdad25d56ba47d20665d479c0ba95ab2c88d9d
shivam kumar <shivamkr19102000@gmail.com>
2023-06-15 15:51:06 +0530

    fixed cors

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin:[process.env.frontend_url],       
-    credentials: true, 
-    methods:['GET', 'POST','PUT','DELETE']
+    origin: process.env.frontend_url,       
+    methods:['GET', 'POST','PUT','DELETE'],
+    credentials: true
 }))
 
 // routers 
